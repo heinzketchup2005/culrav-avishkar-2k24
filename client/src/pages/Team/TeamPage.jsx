f15f029620e1f309fd7947057f164835816fc18b
Mrinal Varshney <varshneymrinal2005@gmail.com>
2024-10-20 18:47:06 +0530

    Add/Team Page UI

--- client/src/pages/Team/TeamPage.jsx
+++ client/src/pages/Team/TeamPage.jsx
@@ -1,3 +1,51 @@
+import React,{useEffect, useState} from "react";
 import PersonCard from "../../Components/Team/PersonCard";
-function Team() {}
+import overlay1 from "../../assets/Overlay1.png";
+import TeamProfile from "@/Components/Team/TeamProfiles";
+import SideBar from "@/Components/Team/SideBar";
+
+function Team() {
+
+    const teams = [
+        "SAC PRESIDENT",
+        "FACULTY INCHARGE",
+        "FACULTY COORDINATOR",
+        "FESTIVAL SECRETARY",
+        "PR TEAM",
+        "WEB TEAM",
+        "DESIGN LEAD",
+        "MEDIA LEADS",
+        "ARTS LEADS",
+        "GNOSIOMANIA LEADS",
+        "DEPARTMENTAL COORDINATOR"
+    ];
+
+    const [selectedTeam, setSelectedTeam] = useState(teams[0]);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    return (
+        <div
+            className="w-full h-full flex flex-row bg-[#111111] overflow-hidden"
+            style={{
+                backgroundImage: `url(${overlay1})`,
+                backgroundSize: 'cover', // Ensure image covers the entire div
+                backgroundPosition: 'center', // Center the image
+                backgroundRepeat: 'no-repeat', // Prevent image repetition
+                width: '100%', // Set the width to 100% of the parent
+                height: '100%' // Set the height of the div
+            }}
+        >
+            <div className="hidden md:block pt-[4%] pl-[3%] md:w-[30%] lg:w-[20%] bg-transparent">
+                <SideBar teams={teams} selectedTeam={selectedTeam} setSelectedTeam={setSelectedTeam}/>
+            </div>
+            <div className="w-full md:w-[80%]">
+                <TeamProfile />
+            </div>
+        </div>
+    );
+}
+
 export default Team;
