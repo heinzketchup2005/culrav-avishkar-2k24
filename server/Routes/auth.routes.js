117e25267e03d13fef3fc4bc562820c100c1729d
Shivansh mishra <125989942+Shivansh-243@users.noreply.github.com>
2024-10-20 18:49:46 +0530

    auth token and routes

---
+++ server/Routes/auth.routes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import SendPasswordToEmail from "../Controllers/auth/authController.forgotPasswordTokenVerification.js";
+import Login from "../Controllers/auth/authController.login.js";
+import Register from "../Controllers/auth/authController.register.js";
+import SendPasswordTokenEmail from "../Controllers/auth/authController.sendForgotPasswordToken.js";
+import VerifyToken from "../Controllers/auth/authController.verifyToken.js";
+const router = express.Router();
+
+// Register route
+router.post("/register", Register);
+
+// Login route
+router.post("/login", Login);
+
+// Verify token route
+router.post("/verify", VerifyToken);
+
+// Forgot password route
+router.post("/sendForgotPasswordToken", SendPasswordTokenEmail);
+router.post("/forgotPasswordVerification", SendPasswordToEmail);
+export default router;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-import express from "express";
 import dotenv from "dotenv";
+import express from "express";
+import rateLimit from "express-rate-limit";
 import connectDB from "./db/connect.js";
 import errorHandler from "./ErrorHandlers/error_handler.js";
 import adminRouter from "./Routes/admin.routes.js";
+import authRoutes from "./Routes/auth.routes.js";
 dotenv.config();
 
 //connect to the mongoDB
@@ -11,6 +13,14 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use("/api/admin", adminRouter);
+
+// auth
+const authLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 10, // Limit to 10 requests per 10 minutes
+  message: "Too many attempts, please try again later.",
+});
+app.use("/api/auth/v1", authLimiter, authRoutes);
 
 //handle every exceptions and errors before starting the server
 app.use(errorHandler);
