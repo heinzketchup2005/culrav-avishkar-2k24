2ed2f0a66d2c353064166754e59ef1eab912daa7
Avinash7390 <avinashk82495@gmail.com>
2024-10-24 15:09:46 +0530

    changed the event Schema

---
+++ server/Controllers/event.controller.js
@@ -0,0 +1,7 @@
+import User from "../Models/user.model";
+import Team from "../Models/team.model.js";
+import Event from "../Models/event.model.js";
+
+const getAllTeamsOfAnEvent = async (req, res, next) => {
+  const { eventId } = req.params;
+};
--- server/Models/event.model.js
+++ server/Models/event.model.js
@@ -1,29 +1,26 @@
 import mongoose from "mongoose";
 
 const EventSchema = new mongoose.Schema(
-    {
-        eventId: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        eventName: {
-            type: String,
-            required: true,
-        },
-        participatingTeams: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Team",
-                required: true,
-            },
-        ],
-        department: {
-            type: String,
-            required: true,
-        },
+  {
+    eventId: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    { timestamps: true }
+    eventName: {
+      type: String,
+      required: true,
+    },
+    participatingTeams: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Team",
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
 const EventModel = mongoose.model("Event", EventSchema);
