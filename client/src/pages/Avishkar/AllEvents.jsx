0f66dc3627e904b1e02ca6776673a5fa76d59c3c
shreyanshgangwar1509 <shreyanshgangwar1@gmail.com>
2024-10-29 13:19:26 +0530

    more responsive culrav and avishkar pages

--- client/src/pages/Avishkar/AllEvents.jsx
+++ client/src/pages/Avishkar/AllEvents.jsx
@@ -1,7 +1,51 @@
+// import EventCard from '@/Components/Avishkar/EventCard';
+// import aviskharbg from '@/images/aviskar-bg.png';
+// import hoverimage from '@/images/hoveraviskhar.png';
+// import cardBg from '@/images/imageavishkar.png';
+// function AvishkarEvents() {
+//     const events = Array(9).fill({
+//         image: cardBg,
+//         hoverImage: hoverimage,
+//         title: "", // Add actual event titles as needed
+//     });
+
+//     return (
+        
+//            <div className="flex flex-col items-center justify-center min-h-screen w-full bg-[#181818] px-4 sm:px-6 lg:px-8  pt-[20%]"
+//            style={{ backgroundImage: `url(${aviskharbg})` }}>
+             
+//             <h1
+//                 className="bg-[#F54E25] font-bionix text-[#FFFAF0] font-bold text-center p-4 mb-8"
+//                 style={{
+//                     fontSize: 'clamp(1.2rem, 4vw, 3rem)', // Responsive font size
+//                     width: 'clamp(150px, 30%, 300px)', // Responsive width
+//                 }}
+//             >
+//                 EVENTS
+//             </h1>
+
+
+
+//             <div className="grid grid-cols-1 w-full xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 4k:grid-cols-5 gap-6 md:gap-10 lg:gap-20 justify-items-center items-center  h-full p-[5%] pb-[10%]">
+//                 {events.map((event, index) => (
+//                     <div key={index} className="relative group flex flex-col items-center w-full h-[450px] sm:h-[450px] md:h-[500px] lg:h-[550px] overflow-hidden">
+//                         {/* Card Container */}
+//                         <EventCard  event={event} />
+                        
+//                     </div>
+//                 ))}
+//             </div>
+//             </div>
+            
+//     );
+// }
+
+// export default AvishkarEvents;
 import EventCard from '@/Components/Avishkar/EventCard';
 import aviskharbg from '@/images/aviskar-bg.png';
 import hoverimage from '@/images/hoveraviskhar.png';
 import cardBg from '@/images/imageavishkar.png';
+
 function AvishkarEvents() {
     const events = Array(9).fill({
         image: cardBg,
@@ -10,33 +54,31 @@ function AvishkarEvents() {
     });
 
     return (
-        
-           <div className="flex flex-col items-center justify-center min-h-screen w-full bg-[#FFF2D5] px-4 sm:px-6 lg:px-8  pt-[20%]"
-           style={{ backgroundImage: `url(${aviskharbg})` }}>
-             
-            <h1 
-                className="bg-[#F54E25] font-bionix text-[#FFFAF0] font-bold text-center p-4 mb-8"
+        <div
+            className="flex flex-col items-center justify-center min-h-screen w-full bg-[#181818] px-4 sm:px-6 lg:px-8 pt-[30%] sm:pt-[20%] md:pt-[15%] lg:pt-[10%]"
+            style={{ backgroundImage: `url(${aviskharbg})` }}
+        >
+            <h1
+                className="bg-[#F54E25] font-bionix text-[#FFFAF0] font-bold text-center mb-8"
                 style={{
-                    fontSize: 'clamp(1.5rem, 4vw, 3rem)', // Responsive font size
-                    width: 'clamp(200px, 30%, 400px)', // Responsive width
+                    fontSize: 'clamp(1.2rem, 3.5vw, 2.5rem)', // Responsive font size
+                    width: 'clamp(140px, 30%, 300px)', // Responsive width
                 }}
             >
                 EVENTS
             </h1>
 
-
-
-            <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 4k:grid-cols-5 gap-6 md:gap-10 lg:gap-20 justify-items-center items-center w-full h-full">
+            <div className="grid grid-cols-1 w-full xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 4k:grid-cols-5 gap-4 md:gap-6 lg:gap-10 justify-items-center h-full p-[5%] pb-[10%]">
                 {events.map((event, index) => (
-                    <div key={index} className="relative group flex flex-col items-center w-full h-[400px] sm:h-[450px] md:h-[500px] lg:h-[550px] overflow-hidden">
-                        {/* Card Container */}
-                        <EventCard  event={event} />
-                        
+                    <div
+                        key={index}
+                        className="relative group flex flex-col items-center w-[90%] h-[300px] sm:h-[300px] md:h-[300px] lg:h-[360px]  p-[5%] overflow-hidden mb-[10%]"
+                    >
+                        <EventCard event={event} />
                     </div>
                 ))}
             </div>
-            </div>
-            
+        </div>
     );
 }
 
--- client/src/pages/Culrav/EventPage.jsx
+++ client/src/pages/Culrav/EventPage.jsx
@@ -1,5 +1,3 @@
-import culravmobilebg from '@/images/avishmobbottom.png';
-import culravhbottom from '@/images/baseblack.png';
 import bullet from '@/images/bullet1.png';
 import layer1 from '@/images/Layer1.png';
 import paint from '@/images/paint.png';
@@ -8,108 +6,111 @@ import wall from '@/images/walls1.png';
 function CulravEvent() {
     return (
         <div
-            className="flex flex-col min-h-screen bg-[#FFFAF0] text-[#181818] w-full bg-fixed overflow-x-hidden pt-[10%] "
+            className="flex flex-col min-h-screen bg-[#FFFAF0] text-[#181818] w-full bg-fixed overflow-x-hidden pt-[8%] sm:pt-[3%] md:pt-[4%] lg:pt-[3%] "
             style={{
-                backgroundImage: `url(${wall})`, // Two layers of background images
-                backgroundRepeat: 'repeat', // Repeat the first image (avishkareventbg) across the page, keep paint centered
-                backgroundPosition: 'top-left', // Position each background layer
-                backgroundSize: '100vw', // Set the size for each background
+                backgroundImage: `url(${wall})`,
+                backgroundRepeat: 'repeat',
+                backgroundPosition: 'top-left',
+                backgroundSize: '100vw',
             }}
         >
-            {/* Paint Background Section */}
+            {/* Layer Background Wrapper for RANGSAAZI and Description */}
             <div
-                className="flex items-center justify-center text-center w-full text-[#FFFAF0] "
+                className="relative flex flex-col items-center justify-center text-center w-[80vw] m-auto "
                 style={{
-                    backgroundImage: `url(${paint})`,
-                    backgroundSize: 'contain',
+                    backgroundImage: `url(${layer1})`,
+                    backgroundSize: 'contain', // Changed to cover
                     backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center',
-                    width: '60vw',
-                    height: '35vw',  // Adjust height to fit the paint design
-                    margin: '0 auto' // Center-align paint background
+                    backgroundPosition: 'center 20vh',
+                    padding: '4rem', // Optional padding for spacing
                 }}
             >
-                <span className="font-bionix text-[4vw] pl-[5%]">KREEDOMANIA</span>
-            </div>
-
-            {/* Content Wrapper */}
-            <div className="flex-grow"
-            >
-                {/* Description Section */}
-                <section className="flex flex-col items-center px-4 py-6 text-center"
-                        style={{
-                        backgroundImage: ` url(${layer1})`, // Two layers of background images
-                        backgroundSize: 'cover',
+                {/* RANGSAAZI Section */}
+                <div
+                    className="flex items-center justify-center w-full text-[#FFFAF0] "
+                    style={{
+                        backgroundImage: `url(${paint})`,
+                        backgroundSize: 'contain',
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'center',
-                        width: '70vw',
-                        height: '35vw',  // Adjust height to fit the paint design
+                        width: '40vw',
+                        height: '23vw',
                         margin: '0 auto'
-                            
-                    }}>
-                    <h2 className="text-[#F54E25] font-bionix text-[3.5vw] uppercase mb-4">Description</h2>
-                    <p className="w-full text-[3vw] text-[#181818] font-bebas"
-                    style={{ wordSpacing: '0.2em' }}>
+                    }}
+                >
+                    <span className="font-bionix text-[3.5vw] sm:text-[3vw] pl-[6%]">RANGSAAZI</span>
+                </div>
+
+                {/* Description Section */}
+                <section className="relative items-center text-center pb-10 pl-[4vw] sm:pb-0 w-full ">
+                    <h2 className="text-[#F54E25] font-bionix text-[3vw] sm:text-[2.5vw] uppercase mb-1">Description</h2>
+                    <p className="text-[2.5vw] sm:text-[2vw] p-[0.8vw] text-[#181818] font-bebas" style={{ wordSpacing: '0.2em' }}>
                         We invite you all to take part in the most popular form of theatre, stage play.
                         Rangmanch brings you Natymanach, stage play. So get up and get ready to mesmerize
                         the world with your theatrics.
                     </p>
                 </section>
-                {/* Rules Section */}
-                <section className="px-4 py-6">
-                    <h2 className="text-[#F54E25] font-bionix text-[3.5vw] uppercase text-center mb-4">Rules</h2>
-                    <ul className="w-full mx-[5%] text-[#181818] font-bebas space-y-4 items-start justify-center flex flex-col">
-                        {[
-                            "The team shall consist of a minimum of 8 and a maximum of 35 people ",
-                            "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
-                            "The time limit for each performance is 20 minutes.",
-                            "Use of harmful props or materials is prohibited.",
-                            "Any kind of offensive content will lead to disqualification.",
-                            "Teams must respect other participants and maintain decorum."
-                        ].map((rule, index) => (
-                            <li key={index} className="flex items-start text-[3vw] mr-[10%]"
-                            style={{ wordSpacing: '0.2em' }}>
-                                <div
-                                    className="mr-4 bg-cover bg-center p-[1vw]"
-                                    style={{
-                                        backgroundImage: `url(${bullet})`,
-                                        width: '0.5em',   // Set bullet width relative to font size
-                                        height: '0.5em'   // Set bullet height relative to font size
-                                    }}
-                                ></div>
-                                {rule}
-                            </li>
-                        ))}
-                    </ul>
+                
+            </div>
 
-                </section>
+            {/* Rules Section */}
+            <section className="pl-[18vw] -mt-[10vh] sm:mt-0 z-10">
+                <h2 className="text-[#F54E25] font-bionix text-[3vw] sm:text-[2.5vw] pr-[15vw] uppercase text-center ">Rules</h2>
+                <ul className="w-full mx-[5%] text-[#181818] font-bebas space-y-4 pr-[10%] items-start justify-center flex flex-col">
+                    {[
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props).",
+                        "The team shall consist of a minimum of 8 and a maximum of 35 people (including people at lights, music and handling props)."
+                    ].map((rule, index) => (
+                        <li key={index} className="flex items-start w-[85%] text-[2.5vw] sm:text-[2vw]  mt-[1.5%]" style={{ wordSpacing: '0.15em' }}>
+                            <div
+                                className="mr-4 bg-cover bg-center p-[0.9vw] sm:p-[0.8vw] mt-[1.5%]"
+                                style={{
+                                    backgroundImage: `url(${bullet})`,
+                                    width: '0.5em',
+                                    height: '0.5em'
+                                }}
+                            ></div>
+                            {rule}
+                        </li>
+                    ))}
+                </ul>
+            </section>
 
-                {/* Register Button */}
-                <div className="flex justify-center py-6">
-                    <button className="bg-[#F54E25] text-[2.5vw] font-bionix hover:bg-orange-600 text-[#FFFAF0] font-bold py-2 px-6">
-                        Register
-                    </button>
-                </div>
+            {/* Register Button */}
+            <div className="flex justify-center py-6 pl-[5vw]">
+                <button className="bg-[#F54E25] text-[2.5vw] font-bionix hover:bg-orange-600 text-[#FFFAF0] font-bold py-2 px-6">
+                    Register
+                </button>
             </div>
 
             {/* Footer Section */}
-            <div
-                className="font-bionix w-full h-full bottom-0 text-center relative overflow-y-hidden overflow-x-hidden"
-            >
-                {/* Mobile Background */}
+            <div className="font-bionix w-full h-full bottom-0 text-center relative overflow-y-hidden overflow-x-hidden">
                 <div
-                    className="absolute inset-0 w-full h-[25vw] bg-cover bg-no-repeat hidden sm:block"
-                    style={{ backgroundImage: `url(${culravhbottom})` }}
-                ></div>
-                <div
-                    className="absolute inset-0 w-full h-[20vw] sm:hidden bg-cover bg-no-repeat"
-                    style={{ backgroundImage: `url(${culravmobilebg})` }}
-                ></div>
+          className="absolute inset-0 w-full bg-floralWhite hidden sm:block -top-4"
+          style={{
+                clipPath: 'polygon(27% 4%, 75% 0%, 100% 15%, 100% 100%, 0 100%, 0 24%)',
+                
+                /* Other styles */
+            }}
+        ></div>
+        <div
+          className="absolute inset-0 w-full bg-[#181818] hidden sm:block  "
+          style={{
+                clipPath: 'polygon(27% 4%, 75% 0%, 100% 15%, 100% 100%, 0 100%, 0 24%)',
+                
+                /* Other styles */
+            }}
+        ></div>
+                <div className="absolute inset-0 w-full h-[20vw] sm:hidden bg-[#181818] bg-cover bg-no-repeat"></div>
 
                 {/* Content overlay */}
-                <div className="relative z-10  ">
+                <div className="relative z-10">
                     <h2 className="uppercase text-[3vw] text-[#F54E25] font-bold mb-4 mt-[5%]">Coordinators</h2>
-                    <div className="flex justify-around font-bold text-[#FFFAF0] text-[2.5vw]">
+                    <div className="flex justify-around font-bold text-[#FFFAF0] text-[2.5vw] pb-4">
                         <div>Person Name</div>
                         <div>Person Name</div>
                         <div>Person Name</div>
@@ -120,4 +121,5 @@ function CulravEvent() {
         </div>
     );
 }
+
 export default CulravEvent;
